0a5e5cc34f2195e07d719d090b38a67fff9c2c11
mohamedsa1990 <mohamedsa1990@gmail.com>
2020-11-12 21:07:54 +0000

    add auth to category admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+  before_filter :authenticate
+
   def index
     @categories = Category.order(id: :desc).all
     # @products = @category.products.order(created_at: :desc)
@@ -21,5 +23,11 @@ class Admin::CategoriesController < ApplicationController
     params.require(:category).permit(
       :name
     )
+  end
+  protected
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['USERNAME'] && password == ENV['PASSWORD']
+    end
   end
 end
